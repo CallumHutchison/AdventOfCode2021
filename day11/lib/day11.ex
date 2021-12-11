7b29bbd6d89a597207d42b508892ad192b59506f
Callum Hutchison <callumhutch98@gmail.com>
2021-12-11 15:50:27 +0000

    Day 11 wip

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -2,14 +2,18 @@ defmodule Day11 do
   @grid_size 10
 
   def run(input_file_name) do
-    grid = load_input(input_file_name)
-    |> IO.inspect()
-
-    Enum.reduce(1..100, {grid, 0}, fn _, {grid, flashes} ->
-      simulate_step(grid)
-      |> then(fn {new_grid, new_flashes} -> {new_grid, flashes + new_flashes} end)
+    grid =
+      load_input(input_file_name)
       |> IO.inspect()
+
+    Enum.reduce(1..10, {grid, 0}, fn i, {grid, flashes} ->
+      simulate_step(grid)
+      |> then(fn {new_grid, new_flashes} ->
+        IO.puts("Flashes on step #{i}: #{new_flashes}")
+        {new_grid, flashes + new_flashes}
+      end)
     end)
+    |> IO.inspect()
   end
 
   defp load_input(file_name) do
@@ -18,48 +22,56 @@ defmodule Day11 do
     |> String.graphemes()
     |> Enum.with_index()
     |> Enum.map(fn {num, i} ->
-      {rem(i, @grid_size), div(i, @grid_size), String.to_integer(num)}
+      {{rem(i, @grid_size), div(i, @grid_size)}, String.to_integer(num)}
     end)
+    |> Map.new()
   end
 
   defp simulate_step(grid) do
-    grid = Enum.map(grid, fn {pos, energy} -> {pos, energy + 1} end)
+    grid = Map.new(grid, fn {pos, energy} -> {pos, energy + 1} end)
 
-    Enum.filter(grid, fn {_, energy} -> energy > 9 end)
-    |> Enum.reduce({grid, 0}, fn {pos, _energy}, {grid, flashes} ->
-      flash(grid, pos)
-      |> then(fn {new_grid, new_flashes} -> {new_grid, new_flashes + flashes} end)
+    Enum.reduce(grid, {grid, 0}, fn
+      {pos, energy}, {grid, flashes} when energy > 9 ->
+        set_energy(grid, pos, energy)
+        |> then(fn {new_grid, new_flashes} -> {new_grid, new_flashes + flashes} end)
+
+      {_, _}, {grid, flashes} ->
+        {grid, flashes}
     end)
+  end
+
+  defp set_energy(grid, pos, energy) do
+    if energy > 9 do
+      flash(grid, pos)
+    else
+      {Map.put(grid, pos, energy), 0}
+    end
   end
 
   # Return the updated grid, and the number of flashes caused in this chain reaction
   defp flash(grid, {x, y}) do
-    if Map.get(grid, {x, y}, -1) < 9 do
-      {Map.update(grid, {x, y}, 0, &(&1 + 1)), 0}
-    else
-      Enum.reduce(
-        # Neighbours
-        [
-          {x - 1, y - 1},
-          {x, y - 1},
-          {x + 1, y - 1},
-          {x - 1, y},
-          {x + 1, y},
-          {x - 1, y + 1},
-          {x, y + 1},
-          {x + 1, y + 1}
-        ],
-        {Map.put(grid, {x, y}, 0), 0},
-        fn
-          {i, j}, {grid, flashes} ->
-            if Map.has_key?(grid, {i, j}) do
-              flash(grid, {i, j})
-              |> then(fn {new_grid, new_flashes} -> {new_grid, new_flashes + flashes} end)
-            else
-              {grid, flashes}
-            end
-        end
-      )
-    end
+    Enum.reduce(
+      # Neighbours
+      [
+        {x - 1, y - 1},
+        {x, y - 1},
+        {x + 1, y - 1},
+        {x - 1, y},
+        {x + 1, y},
+        {x - 1, y + 1},
+        {x, y + 1},
+        {x + 1, y + 1}
+      ],
+      {Map.put(grid, {x, y}, 0), 1},
+      fn
+        {i, j}, {grid, flashes} ->
+          if Map.has_key?(grid, {i, j}) and grid[{i, j}] != 0 do
+            set_energy(grid, {i, j}, grid[{i, j}] + 1)
+            |> then(fn {new_grid, new_flashes} -> {new_grid, new_flashes + flashes} end)
+          else
+            {grid, flashes}
+          end
+      end
+    )
   end
 end
