fa34c997f214450745e2e600f08fc98ea94df920
Callum Hutchison <callumhutch98@gmail.com>
2021-12-12 10:17:59 +0000

    wip

--- day12/lib/day12.ex
+++ day12/lib/day12.ex
@@ -1,23 +1,10 @@
 defmodule Day12 do
   def run(input_file_name) do
-    graph = load_input(input_file_name)
-
-    part_one =
-      traverse(graph, false)
-      |> IO.inspect()
-      |> Enum.filter(&(List.last(&1) == "end"))
-      |> Enum.count()
-
-    part_two =
-      traverse(graph, true)
-      |> IO.inspect()
-      |> Enum.filter(&(List.last(&1) == "end"))
-      |> Enum.count()
-
-    {part_one, part_two}
+    graph = load_input(input_file_name) |> IO.inspect()
+    {count_paths(graph, false), count_paths(graph, true)}
   end
 
-  def load_input(file_name) do
+  defp load_input(file_name) do
     File.read!("lib/#{file_name}.txt")
     |> String.split(~r/\R/)
     |> Enum.map(&String.split(&1, "-"))
@@ -26,42 +13,57 @@ defmodule Day12 do
     end)
   end
 
-  def add_connection(graph = %{}, start_node, end_node) do
+  defp add_connection(graph = %{}, start_node, end_node) do
     Map.update(graph, start_node, [end_node], &([end_node] ++ &1))
     |> Map.update(end_node, [start_node], &([start_node] ++ &1))
   end
 
   # Big caves have their names written in uppercase
-  def is_big_cave(cave_name) do
+  defp is_big_cave(cave_name) do
     String.upcase(cave_name) == cave_name
   end
 
-  def traverse(graph = %{}, allow_small_revisits) do
+  defp count_paths(graph = %{}, allow_small_revisits) do
+    traverse(graph, allow_small_revisits)
+      |> IO.inspect()
+      |> Enum.filter(&(List.last(&1) == "end"))
+      |> Enum.count()
+  end
+
+  defp traverse(graph = %{}, allow_small_revisits) do
     traverse(graph, "start", MapSet.new(), allow_small_revisits)
   end
 
-  def traverse(_, "end", _, _) do
+  defp traverse(_, "end", _, _) do
     [["end"]]
   end
 
-  def traverse(graph = %{}, current_node, visited_nodes = %MapSet{}, allow_small_revisits) do
+  defp traverse(graph = %{}, current_node, visited_nodes = %MapSet{}, allow_small_revisits) do
     Map.get(graph, current_node)
-    |> Enum.filter(
-      &(&1 != "start" &&
-          (is_big_cave(&1) or !MapSet.member?(visited_nodes, &1) or allow_small_revisits))
-    )
+    |> Enum.filter(&can_visit_cave(&1, visited_nodes, allow_small_revisits))
     |> Enum.map(fn node ->
       traverse(
         graph,
         node,
-        MapSet.put(visited_nodes, current_node),
-        if(allow_small_revisits,
-          do: current_node == "start" or is_big_cave(current_node) or !MapSet.member?(visited_nodes, current_node),
-          else: false
-        )
+        if(is_big_cave(current_node), do: visited_nodes, else: MapSet.put(visited_nodes, current_node)),
+        can_still_revisit(current_node, visited_nodes, allow_small_revisits)
       )
       |> Enum.map(fn path -> [current_node | path] end)
     end)
     |> Enum.reduce([], &Enum.reduce(&1, &2, fn path, acc -> [path | acc] end))
+  end
+
+  defp can_visit_cave("start", _, _), do: false
+
+  defp can_visit_cave(node, visited_nodes, allow_small_revisits) do
+    !MapSet.member?(visited_nodes, node) or allow_small_revisits
+  end
+
+  # After leaving start, we can still revisit
+  defp can_still_revisit("start", _, allow_small_revisits), do: allow_small_revisits
+  defp can_still_revisit(_, _, false), do: false
+  defp can_still_revisit(node, visited_nodes, true) do
+    IO.puts("#{node}, #{!MapSet.member?(visited_nodes, node)}")
+    !MapSet.member?(visited_nodes, node)
   end
 end
