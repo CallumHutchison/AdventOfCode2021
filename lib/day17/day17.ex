7b82cdbe315cd5c93e0670d2aee6751aa5085812
Callum Hutchison <callumhutch98@gmail.com>
2021-12-17 23:00:16 +0000

    Reasonable bounds for day 17

--- lib/day17/day17.ex
+++ lib/day17/day17.ex
@@ -1,15 +1,16 @@
 defmodule Day17 do
   def run(input_file_name) do
-    target = load_input(input_file_name)
+    {min_x..max_x, min_y.._max_y} = target = load_input(input_file_name)
 
-    trajectories = Enum.reduce(0..1000, [], fn x, acc ->
-      Enum.reduce(-100..100, acc, fn y, acc ->
-        if does_trajectory_reach_target?({0, 0}, {x, y}, target),
-           do: [{x, y} | acc],
-           else: acc
+    trajectories =
+      Enum.reduce(get_triangular_root(min_x)..max_x, [], fn x, acc ->
+        Enum.reduce(min_y..abs(min_y), acc, fn y, acc ->
+          if does_trajectory_reach_target?({0, 0}, {x, y}, target),
+            do: [{x, y} | acc],
+            else: acc
+        end)
       end)
-    end)
-    
+
     heights_achieved = Enum.map(trajectories, fn {_, y} -> get_max_height(0, y) end)
 
     {Enum.max(heights_achieved), Enum.count(trajectories)}
@@ -45,4 +46,8 @@ defmodule Day17 do
   # Decreases by 1 per step, the velocities act as triangle numbers
   def get_max_height(start, velocity),
     do: start + ((Integer.pow(velocity, 2) + velocity) |> div(2))
+
+  def get_triangular_root(n) do
+    div(round(:math.sqrt(8 * n + 1) - 1), 2)
+  end
 end
